99780b5f6f5a5f2215116dc5e63ae942763d1309
Paul Pukach <pavlopukach@gmail.com>
2019-02-10 01:47:33 +0200

    Implemented songs average difficulty endpoint

--- tests/test_avg_difficulty.py
+++ tests/test_avg_difficulty.py
@@ -10,4 +10,8 @@ class TestAverageDifficulty:
 
     def test_level_argument_works(self, client, sample_songs):
         """ Level parameter should filter songs starting from a certain level """
-        pass
+        response = client.get('/songs/avg/difficulty?level=10')
+        assert response.status_code == 200
+        filtered_songs = list(filter(lambda x: x['level'] >= 10, sample_songs))
+        assert response.json['data']['avg_difficulty'] == sum(
+            song['difficulty'] for song in filtered_songs) / len(filtered_songs)
